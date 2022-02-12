2d983dd30de26e0c437d7e23da7f207792b4cb06
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-12 18:29:29 +0900

    main

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf-8');
+const style_css = fs.readFileSync('./style.css', 'utf-8');
 
 var server = http.createServer(getFromClient);
 
@@ -10,15 +12,46 @@ server.listen(3000);
 console.log('Server start!');
 
 function getFromClient(request, response) {
-    var content = ejs.render(index_page,{
-        title:"Indexページ",
-        content:"これはテンプレートを使ったサンプルページです。",
-    });
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    response.write(content);
-    response.end();
+    var url_parts = url.parse(request.url);
+    switch (url_parts.pathname) {
+
+        case '/':
+            var content = ejs.render(index_page, {
+                title: "Indexページ",
+                content: "これはテンプレートを使ったサンプルページです。",
+            });
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.write(content);
+            response.end();
+            break;
+
+        case '/style.css':
+            response.writeHead(200, {
+                'Content-Type': 'text/css'
+            });
+            response.write(style_css);
+            response.end();
+            break;
+
+        default:
+            response.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            response.end('no page...');
+            break;
+    }
+
+    // var content = ejs.render(index_page,{
+    //     title:"Indexページ",
+    //     content:"これはテンプレートを使ったサンプルページです。",
+    // });
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/html'
+    // });
+    // response.write(content);
+    // response.end();
 
     // fs.readFile('./index.html', 'utf-8',
     //     (error, data) => {
