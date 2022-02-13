9fcc3cc6db96a37f86b192021eec2276d5a705cd
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-13 20:05:12 +0900

    main

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf-8');
 const other_page = fs.readFileSync('./other.ejs', 'utf-8');
@@ -14,6 +15,7 @@ console.log('Server start!');
 
 function getFromClient(request, response) {
     var url_parts = url.parse(request.url, true);
+
     switch (url_parts.pathname) {
 
         case '/':
@@ -21,32 +23,34 @@ function getFromClient(request, response) {
             //     title: "Indexページ",
             //     content: "これはテンプレートを使ったサンプルページです。",
             // });
-            var content = "これはIndexページです。";
-            var query = url_parts.query;
-            if (query.msg != undefined) {
-                content += 'あなたは、「' + query.msg + '」と送りました。';
-            }
-            var content = ejs.render(index_page, {
-                title: "index",
-                content: content,
-            });
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write(content);
-            response.end();
+            // var content = "これはIndexページです。";
+            // var query = url_parts.query;
+            // if (query.msg != undefined) {
+            //     content += 'あなたは、「' + query.msg + '」と送りました。';
+            // }
+            // var content = ejs.render(index_page, {
+            //     title: "index",
+            //     content: content,
+            // });
+            // response.writeHead(200, {
+            //     'Content-Type': 'text/html'
+            // });
+            // response.write(content);
+            // response.end();
+            response_index(request, response);
             break;
 
         case '/other':
-            var content = ejs.render(other_page, {
-                title: "Index",
-                content: "これは新しく用意したページです。",
-            });
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write(content);
-            response.end();
+            // var content = ejs.render(other_page, {
+            //     title: "Index",
+            //     content: "これは新しく用意したページです。",
+            // });
+            // response.writeHead(200, {
+            //     'Content-Type': 'text/html'
+            // });
+            // response.write(content);
+            // response.end();
+            response_other(request, response);
             break;
 
         case '/style.css':
@@ -83,6 +87,56 @@ function getFromClient(request, response) {
     //         response.write(data);
     //         response.end();
     //     });
+}
+
+function response_index(request, response) {
+    var msg = "これはIndexページです。"
+    var content = ejs.render(index_page, {
+        title: "Index",
+        content: msg,
+    });
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.write(content);
+    response.end();
+}
+
+function response_other(request, response) {
+    var msg = "これはOtherページです。"
+
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        request.on('end', () => {
+            var post_data = qs.parse(body);
+            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
+            var content = ejs.render(other_page, {
+                title: "Other",
+                content: msg,
+            });
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.write(content);
+            response.end();
+        });
+    } else {
+        var msg = "ページがありません。";
+        var content = ejs.render(other_page, {
+            title: "Other",
+            content: msg,
+        });
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.write(content);
+        response.end();
+    }
 }
 
 // var server = http.createServer(
