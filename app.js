283abacc78bb117dab64b7850691004a92d0ce94
MakotoTaguchi <taguchimakoto@icloud.com>
2022-02-14 19:13:07 +0900

    main

--- app.js
+++ app.js
@@ -19,37 +19,10 @@ function getFromClient(request, response) {
     switch (url_parts.pathname) {
 
         case '/':
-            // var content = ejs.render(index_page, {
-            //     title: "Indexページ",
-            //     content: "これはテンプレートを使ったサンプルページです。",
-            // });
-            // var content = "これはIndexページです。";
-            // var query = url_parts.query;
-            // if (query.msg != undefined) {
-            //     content += 'あなたは、「' + query.msg + '」と送りました。';
-            // }
-            // var content = ejs.render(index_page, {
-            //     title: "index",
-            //     content: content,
-            // });
-            // response.writeHead(200, {
-            //     'Content-Type': 'text/html'
-            // });
-            // response.write(content);
-            // response.end();
             response_index(request, response);
             break;
 
         case '/other':
-            // var content = ejs.render(other_page, {
-            //     title: "Index",
-            //     content: "これは新しく用意したページです。",
-            // });
-            // response.writeHead(200, {
-            //     'Content-Type': 'text/html'
-            // });
-            // response.write(content);
-            // response.end();
             response_other(request, response);
             break;
 
@@ -68,32 +41,9 @@ function getFromClient(request, response) {
             response.end('no page...');
             break;
     }
-
-    // var content = ejs.render(index_page,{
-    //     title:"Indexページ",
-    //     content:"これはテンプレートを使ったサンプルページです。",
-    // });
-    // response.writeHead(200, {
-    //     'Content-Type': 'text/html'
-    // });
-    // response.write(content);
-    // response.end();
-
-    // fs.readFile('./index.html', 'utf-8',
-    //     (error, data) => {
-    //         response.writeHead(200, {
-    //             'Content-Type': 'text/html'
-    //         });
-    //         response.write(data);
-    //         response.end();
-    //     });
 }
 
 var data = {
-    // 'Taro': '09-999-999',
-    // 'Hanako': '080-888-888',
-    // 'Sachiko': '070-777-777',
-    // 'Ichiro': '060-666-666'
     msg: 'no message...'
 };
 
@@ -112,18 +62,6 @@ function response_index(request, response) {
     } else {
         write_index(request, response);
     }
-    // var msg = "これはIndexページです。"
-    // var content = ejs.render(index_page, {
-    //     title: "Index",
-    //     content: msg,
-    //     data: data,
-    //     filename: 'data_item'
-    // });
-    // response.writeHead(200, {
-    //     'Content-Type': 'text/html'
-    // });
-    // response.write(content);
-    // response.end();
 }
 
 function write_index(request,response) {
@@ -155,59 +93,4 @@ function response_other(request, response) {
     });
     response.write(content);
     response.end();
-
-    // if (request.method == 'POST') {
-    //     var body = '';
-
-    //     request.on('data', (data) => {
-    //         body += data;
-    //     });
-
-    //     request.on('end', () => {
-    //         var post_data = qs.parse(body);
-    //         msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-    //         var content = ejs.render(other_page, {
-    //             title: "Other",
-    //             content: msg,
-    //         });
-    //         response.writeHead(200, {
-    //             'Content-Type': 'text/html'
-    //         });
-    //         response.write(content);
-    //         response.end();
-    //     });
-    // } else {
-    //     var msg = "ページがありません。";
-    //     var content = ejs.render(other_page, {
-    //         title: "Other",
-    //         content: msg,
-    //     });
-    //     response.writeHead(200, {
-    //         'Content-Type': 'text/html'
-    //     });
-    //     response.write(content);
-    //     response.end();
-    // }
 }
-
-// var server = http.createServer(
-//     (request, response) => {
-//         // response.end('Hello Node.js!');
-//         // response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
-//         // response.setHeader('Content-Type','text/html');
-//         // response.write('<!DOCTYPE html><html lang="ja">');
-//         // response.write('<head><meta charset="utf-8">');
-//         // response.write('<title>Hello</title></head>');
-//         // response.write('<body><h1>Hello Node.js!</h1>');
-//         // response.write('<p>This is Node.js sample page.</p>');
-//         // response.write('<p>これはNode.jsのサンプルページです。</p>','utf-8');
-//         // response.write('</body></html>');
-//         // response.end();
-//         fs.readFile('./index.html','utf-8',
-//         (error,data)=>{
-//             response.writeHead(200,{'Content-Type':'text/html'});
-//             response.write(data);
-//             response.end();
-//         });
-//     }
-// );
